8fd0c0067eb55491627abfc555b8520a589b5433
Jianan <yontbear@users.noreply.github.com>
2019-10-19 16:24:50 +0800

    Update example-smart-app.js
    modify code to add weight

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -38,6 +38,7 @@
           }
 
           var height = byCodes('8302-2');
+          var weight = byCodes('29463-7');
           var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
           var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
           var hdl = byCodes('2085-9');
@@ -80,6 +81,7 @@
       gender: {value: ''},
       birthdate: {value: ''},
       height: {value: ''},
+      weight:{value:''},
       systolicbp: {value: ''},
       diastolicbp: {value: ''},
       ldl: {value: ''},
@@ -123,6 +125,7 @@
     $('#gender').html(p.gender);
     $('#birthdate').html(p.birthdate);
     $('#height').html(p.height);
+    $('#weight').html(p.weight);
     $('#systolicbp').html(p.systolicbp);
     $('#diastolicbp').html(p.diastolicbp);
     $('#ldl').html(p.ldl);
